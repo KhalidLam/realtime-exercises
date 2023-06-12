4e362386b49533a72c091b3ab1c879ee22bded7b
Lamsadi khalid <khalid@lafactory.co>
2023-06-12 10:13:07 +0100

    create example of real time chat using polling method using setTimeout and requestAnimationFrame

--- polling/exercise/backend/server.js
+++ polling/exercise/backend/server.js
@@ -9,8 +9,8 @@ const getMsgs = () => Array.from(msg).reverse();
 
 // feel free to take out, this just seeds the server with at least one message
 msg.push({
-  user: "brian",
-  text: "hi",
+  user: "khalid",
+  text: "Hey there",
   time: Date.now(),
 });
 
@@ -21,13 +21,23 @@ app.use(bodyParser.json());
 app.use(express.static("frontend"));
 
 app.get("/poll", function (req, res) {
-  // use getMsgs to get messages to send back
-  // write code here
+  return res.json({
+    msg: getMsgs(),
+  });
 });
 
 app.post("/poll", function (req, res) {
-  // add a new message to the server
-  // write code here
+  const { user, text } = req.body;
+
+  msg.push({
+    user,
+    text,
+    time: new Date(),
+  });
+
+  return res.json({
+    status: "ok",
+  });
 });
 
 // start the server
